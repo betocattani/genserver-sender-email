49236bc9a8413f969f0f1bc330844a44fa3c7b70
Cattani <contato.cattani@gmail.com>
2021-03-29 00:12:53 -0300

    creates EmailTaskSupervisor and using async_stream_nolink

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,7 @@
 emails = [
   "joe@erlang.com",
   "ragnar@vikings.com",
+  "cattani@mail.com",
   "darrel@pantera.com",
   "segovia@guitar.com"
 ]
--- lib/sender.ex
+++ lib/sender.ex
@@ -1,4 +1,7 @@
 defmodule Sender do
+  def send_email("cattani@mail.com" = email),
+    do: raise(raise "Oops, couldn't send email to #{email}!")
+
   def send_email(email) do
     Process.sleep(3000)
     IO.puts("Email to #{email} sent")
@@ -6,8 +9,8 @@ defmodule Sender do
   end
 
   def notify_all(emails) do
-    emails
-    |> Task.async_stream(&send_email/1)
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
     |> Enum.to_list()
   end
 end
